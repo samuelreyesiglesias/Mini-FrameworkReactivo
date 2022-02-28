05bb997df8106113235fe68f68e0778a211d8bd9
Samuel <samuelreyesiglesias@gmail.com>
2022-02-28 11:11:18 -0600

    MiniFrameworkRectivo

--- MFR.js
+++ MFR.js
@@ -1,31 +1,71 @@
-class mfr{
-    constructor(options){
-        this.origen = options.data();
+class mfr {
+    //dependencias
+    deps = new Map();
 
+    constructor(options) {
+        this.origen = options.data();
+        const self = this;
         //destino
-        this.$data  = new Proxy(this.origen);
+        this.$data = new Proxy(this.origen, {
+            get(target, name) {
+                if (Reflect.has(target, name)) {
+                    self.track(target, name);
+                    return Reflect.get(target, name);
+                }
+                console.warn("No existe el atributo: " + name);
+                return "";
+                console.log(target, name)
+            },
+            set(target, name, value) {
+                Reflect.set(target, name, value);
+                self.trigger(name);
+                return false;
+            }
+
+        })
     }
 
-    mount(){
+    track(target, name) {
+        if (!this.deps.has(name)) {
+            const effect = () => {
+                document.querySelectorAll(`*[m-text=${name}]`).forEach(element => {
+                    this.mText(element, target, name);
+                });
+            }
+            this.deps.set(name, effect);
+        }
+    }
+
+    trigger(name) {
+        const effect = this.deps.get(name);
+        effect();
+    }
+
+    mount() {
         document.querySelectorAll("*[m-text]").forEach(element => {
-            this.mText(element, this.origen, element.getAttribute("m-text"));
+            this.mText(element, this.$data, element.getAttribute("m-text"));
         });
         document.querySelectorAll("*[m-model]").forEach(element => {
-            this.mText(element, this.origen, element.getAttribute("m-model"));
+            const name = element.getAttribute("m-model");
+            this.mModel(element, this.$data, name);
+            element.addEventListener("input", () => {
+                Reflect.set(this.$data, name, element.value);
+                //this.$data[name] = e.target.value;
+            });
         });
     }
 
-    mText(element, origen, name){ 
-        element.innerText = origen[name];
+    mText(element, target, name) {
+        element.innerText = Reflect.get(target, name);
     }
 
-    mModel(element, origen, name){
-        element.value = origen[name];
+    mModel(element, target, name) {
+        element.value = Reflect.get(target, name);
     }
 }
 
 var mf = {
-    createApp(options){
+    createApp(options) {
         return new mfr(options);
     }
 }
